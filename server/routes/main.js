c83bf84684ba6469a07b9b81657525001739479c
Sachin Jha <sjha04180@gmail.com>
2025-06-30 12:50:44 +0530

    Update main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,12 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 
-router.get("/", (req, res) => {
-  res.send({
-    activeStatus: true,
-    error: false,
-  });
-});
+
 
 
 /**
