29247bc089c420b446740e665abc16e695b7f5f3
Sachin Jha <sjha04180@gmail.com>
2025-06-30 14:44:45 +0530

    layouts

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,7 +4,7 @@ const Post = require('../models/Post');
 const User = require('../models/User');;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const adminLayout = "../views/Layouts/admin"
+const adminLayout = "../views/layouts/admin"
 const jwtSecret = process.env.JWT_SECRET;
 
 /**
