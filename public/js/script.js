748f1452be9e796f717bb62a7a34bf41e6167f53
Sachin Jha <sjha04180@gmail.com>
2025-06-30 15:44:04 +0530

    Added eye button for passwords

--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,27 @@ function toggleTheme() {
     
   }
 }
+
+//For Login password toggle
+const passwordInput = document.getElementById("passwordInput");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+  const type = passwordInput.getAttribute("type");
+  passwordInput.setAttribute("type", type === "password" ? "text" : "password");
+
+  // Optionally change the icon/text
+  togglePassword.textContent = type === "password" ? "🙈" : "👁️";
+});
+
+//For Login password toggle
+const passwordInput2 = document.getElementById("passwordInput2");
+const togglePassword2 = document.getElementById("togglePassword2");
+
+togglePassword2.addEventListener("click", () => {
+  const type = passwordInput2.getAttribute("type");
+  passwordInput2.setAttribute("type", type === "password" ? "text" : "password");
+
+  // Optionally change the icon/text
+  togglePassword2.textContent = type === "password" ? "🙈" : "👁️";
+});
